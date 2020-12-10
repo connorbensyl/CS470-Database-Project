6489b1b79c78316fdeeeb5451fa7a0d2fa63cdf4
connorbensyl <48489225+connorbensyl@users.noreply.github.com>
2020-12-10 06:27:26 -0600

    added queries and buttons but need connection to database to proceed anymore

--- Source Code/DBgui.py
+++ Source Code/DBgui.py
@@ -10,14 +10,12 @@ import PIL
 def submitact():
     user = Username.get()
     passw = password.get()
-
-    print(f"The name entered by you is {user} {passw}")
-
     logintodb(user, passw)
+    print(f"The name entered by you is {user} {passw}")
 
 
 def logintodb(user, passw):
-    # If paswword is enetered by the
+    # If paswword is entered by the
     # user
     if passw:
         db = mysql.connector.connect(host="kc-sce-appdb01",
@@ -29,27 +27,37 @@ def logintodb(user, passw):
         # If no password is enetered by the
     # user
     else:
-        db = mysql.connector.connect(host="localhost",
+        db = mysql.connector.connect(host="kc-sce-appdb01",
                                      user=user,
-                                     db="zoo")
+                                     db="cjbdxd")
         cursor = db.cursor()
 
-        # A Table in the database
-    savequery = "select * from STUDENT"
+    #     # A Table in the database
+    # savequery = "select * from ANIMAL"
+    #
+    # try:
+    #     cursor.execute(savequery)
+    #     myresult = cursor.fetchall()
+    #
+    #     # Printing the result of the
+    #     # query
+    #     for x in myresult:
+    #         print(x)
+    #     print("Query Excecuted successfully")
+    #
+    # except:
+    #     db.rollback()
+    #     print("Error occured")
 
-    try:
-        cursor.execute(savequery)
-        myresult = cursor.fetchall()
-
-        # Printing the result of the
-        # query
-        for x in myresult:
-            print(x)
-        print("Query Excecuted successfully")
-
-    except:
-        db.rollback()
-        print("Error occured")
+    animal_query = "SELECT * FROM animal"
+    employee_query = "SELECT * FROM employee"
+    supervisor_query = "SELECT * FROM employee WHERE super_id IN(SELECT habitat_number FROM habitat)"
+    animal_btn = tk.Button(root, text="animals", width=200, length=200, bg='white',
+                           command=cursor.execute(animal_query))
+    employee_btn = tk.Button(root, text="employees", width=200, length=200, bg='white',
+                             command=cursor.execute(employee_query))
+    supervisor_btn = tk.Button(root, text="employees", width=200, length=200, bg='white',
+                               command=cursor.execute(supervisor_query))
 
 
 root = tk.Tk()
